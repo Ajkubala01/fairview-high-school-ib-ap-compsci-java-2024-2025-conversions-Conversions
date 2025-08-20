d37f2162df6cf8a8ae2d3c638e30dba47e90863a
Ajkubala01 <ajkubala01@bvsd.org>
2025-08-20 20:28:14 +0000

    Decided on conversions

--- Conversions.java
+++ Conversions.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 /**
  * This application converts between various units of measurement.
  *
- * @author Tim Gesell
- * @version 8/21/2022
+ * @author Adam Kubala
+ * @version 8/20/2025
  */
 public class Conversions
 {
@@ -17,6 +17,10 @@ public class Conversions
         System.out.println("4. Meters to Feet");
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
+        System.out.println("7. Milimeters to inches");
+        System.out.println("8. Inches to Milimeters");
+        System.out.println("9. Feet to Kilometers");
+        System.out.println("10. Kilometers to Feet");
         //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
         //You can add even more if you would like.
 
@@ -33,8 +37,77 @@ public class Conversions
         }
         if (selection == 2)
         {
-            //add the code to ask the user to enter Fahrenheit and then convert to Celsius
+            System.out.println("Enter Fahrenheit: ");
+            double fahrenheit = keyboard.nextDouble();
+            keyboard.nextLine();
+            double celsius = fahrenheit * (5.0 / 9.0) - 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
         }
+        if (selection == 3)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        if (selection == 4)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        if (selection == 5)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        if (selection == 6)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        if (selection == 7)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        if (selection == 8)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        if (selection == 9)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        if (selection == 10)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        
         //add more if statements here.
     }
 }
